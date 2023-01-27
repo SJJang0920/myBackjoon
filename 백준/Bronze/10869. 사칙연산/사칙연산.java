3633dfae5dfaa98ea39a06b3560301a4165f8559
Morning <112756085+SJJang0920@users.noreply.github.com>
2023-01-27 19:51:43 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String data = br.readLine();
+		int num1 = Integer.parseInt(data.split(" ")[0]);
+		int num2 = Integer.parseInt(data.split(" ")[1]);
+			
+		System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		System.out.println(num1/num2);
+		System.out.println(num1%num2);
+	}
+}
