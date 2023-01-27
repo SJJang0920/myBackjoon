d6cb415e9059d197c19b9c74d8dd5f65ec3e30ae
Morning <112756085+SJJang0920@users.noreply.github.com>
2023-01-27 19:51:24 +0900

    [Bronze V] Title: A+B - 4, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		while(true) {
+			String line = br.readLine();
+			if(line==null) {
+				break;
+			}	
+			String[] resultarr = line.split(" ");
+			System.out.println(Integer.parseInt(resultarr[0])+Integer.parseInt(resultarr[1]));			
+		}	
+	}
+}
