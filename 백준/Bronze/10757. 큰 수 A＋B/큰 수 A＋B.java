39905e0c35d44653fb149290b945faaee77f22b1
Morning <112756085+SJJang0920@users.noreply.github.com>
2023-02-07 16:56:51 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 248 ms, Memory: 17420 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BigInteger A = new BigInteger(st.nextToken());
+		BigInteger B = new BigInteger(st.nextToken());
+		BigInteger ans = A.add(B);
+		bw.write(ans+"\n");
+		bw.flush();
+		bw.close();
+	}
+}
